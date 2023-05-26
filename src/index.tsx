08bd3a130da9b4c54e5a734a30913b183c16e411
Jalisa Bonilla <34517248+jbonilla8@users.noreply.github.com>
2023-05-26 14:01:55 -0400

    added some fonts and implemented expo splash screen/font loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,62 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { Game, Home } from "@screens";
+import React, { useState, useEffect, useCallback } from "react";
+import { loadAsync } from "expo-font";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  IBMPlexMono_400Regular,
+  IBMPlexMono_700Bold,
+} from "@expo-google-fonts/ibm-plex-mono";
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
+SplashScreen.hideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await loadAsync({
+          IBMPlexMono_400Regular,
+          IBMPlexMono_700Bold,
+        } as any);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      {/* <Image source={require("@assets/icon.png")} /> */}
-      <Home />
-      <Game />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text
+        style={{ fontSize: 40, fontFamily: "IBMPlexMono_400Regular" }}
+        onPress={() => alert(true)}
+      >
+        Hello World
+      </Text>
     </View>
   );
 }
