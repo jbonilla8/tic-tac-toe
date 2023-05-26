4134b98afefcaef861fee6e5bb587f3a43eabdfd
Jalisa Bonilla <34517248+jbonilla8@users.noreply.github.com>
2023-05-26 14:10:55 -0400

    spread the props

--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -1,10 +1,13 @@
 import { View, Text as NativeText } from "react-native";
 import React from "react";
 
-export default function Text({ children, style }) {
+export default function Text({ children, style, ...props }) {
   return (
     <View>
-      <NativeText style={[{ fontFamily: "IBMPlexMono_400Regular" }, style]}>
+      <NativeText
+        {...props}
+        style={[{ fontFamily: "IBMPlexMono_400Regular" }, style]}
+      >
         {children}
       </NativeText>
     </View>
