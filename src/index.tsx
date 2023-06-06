993ef2bfad8cb10f1c8ca76914e3ee9b1044df38
Jalisa Bonilla <34517248+jbonilla8@users.noreply.github.com>
2023-06-06 13:00:58 -0400

    added navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,69 +1,57 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { loadAsync } from "expo-font";
-import { StyleSheet, View } from "react-native";
-import { Text } from "@components";
-import {
-  IBMPlexMono_400Regular,
-  IBMPlexMono_700Bold,
-} from "@expo-google-fonts/ibm-plex-mono";
-import * as SplashScreen from "expo-splash-screen";
-SplashScreen.preventAutoHideAsync();
-SplashScreen.hideAsync();
+import { View } from "react-native";
+import Navigator from "@config/navigator";
+// import {
+//   IBMPlexMono_400Regular,
+//   IBMPlexMono_700Bold,
+// } from "@expo-google-fonts/ibm-plex-mono";
+// import * as SplashScreen from "expo-splash-screen";
+// SplashScreen.preventAutoHideAsync();
+// SplashScreen.hideAsync();
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
+  //   const [appIsReady, setAppIsReady] = useState(false);
 
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        await loadAsync({
-          IBMPlexMono_400Regular,
-          IBMPlexMono_700Bold,
-        } as any);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
-        // await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
+  //   useEffect(() => {
+  //     async function prepare() {
+  //       try {
+  //         // Pre-load fonts, make any API calls you need to do here
+  //         await loadAsync({
+  //           IBMPlexMono_400Regular,
+  //           IBMPlexMono_700Bold,
+  //         } as any);
+  //         // Artificially delay for two seconds to simulate a slow loading
+  //         // experience. Please remove this if you copy and paste the code!
+  //         // await new Promise(resolve => setTimeout(resolve, 2000));
+  //       } catch (e) {
+  //         console.warn(e);
+  //       } finally {
+  //         // Tell the application to render
+  //         setAppIsReady(true);
+  //       }
+  //     }
 
-    prepare();
-  }, []);
+  //     prepare();
+  //   }, []);
 
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
+  //   const onLayoutRootView = useCallback(async () => {
+  //     if (appIsReady) {
+  //       // This tells the splash screen to hide immediately! If we call this after
+  //       // `setAppIsReady`, then we may see a blank screen while the app is
+  //       // loading its initial state and rendering its first pixels. So instead,
+  //       // we hide the splash screen once we know the root view has already
+  //       // performed layout.
+  //       await SplashScreen.hideAsync();
+  //     }
+  //   }, [appIsReady]);
 
-  if (!appIsReady) {
-    return null;
-  }
+  //   if (!appIsReady) {
+  //     return null;
+  //   }
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={{ fontSize: 40 }} onPress={() => alert(true)}>
-        Hello World <Text weight="700">hey</Text>
-      </Text>
-    </View>
+    // <View onLayout={onLayoutRootView}>
+    <Navigator />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button } from "react-native";
+import React, { ReactElement } from "react";
 import styles from "./home.styles";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack/lib/typescript/src/types";
+import { StackNavigatorParams } from "@config/navigator";
 
-export default function Home() {
+type HomeProps = {
+  navigation: NativeStackNavigationProp<StackNavigatorParams, "Home">;
+};
+
+export default function Home({ navigation }: HomeProps): ReactElement {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Button
+        title="Game"
+        onPress={() => {
+          navigation.navigate("Game", { gameId: "455" });
+        }}
+      />
     </View>
   );
 }
