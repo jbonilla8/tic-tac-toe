dd1202170f457342720ecefeab1f04c72f3e335e
Jalisa Bonilla <34517248+jbonilla8@users.noreply.github.com>
2023-06-22 11:43:35 -0400

    created settings page

--- src/config/navigator.tsx
+++ src/config/navigator.tsx
@@ -1,25 +1,54 @@
-import { View, Text } from "react-native";
 import React, { ReactElement } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Home, SinglePlayerGame } from "@screens";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationOptions,
+} from "@react-navigation/native-stack";
+import { Home, SinglePlayerGame, Settings } from "@screens";
+import { colors } from "@utils";
 
 export type StackNavigatorParams = {
   Home: undefined;
   SinglePlayerGame: undefined;
+  Settings: undefined;
 };
 
 const Stack = createNativeStackNavigator<StackNavigatorParams>();
 
+const navigatorOptions: NativeStackNavigationOptions = {
+  headerStyle: {
+    backgroundColor: colors.teal,
+  },
+  headerTintColor: colors.lightGreen,
+  headerTitleStyle: {
+    fontFamily: "IBMPlexMono_700Bold",
+    fontSize: 20,
+  },
+  headerBackTitleStyle: {
+    fontFamily: "IBMPlexMono_700Bold",
+    fontSize: 14,
+  },
+};
+
 export default function Navigator(): ReactElement {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="SinglePlayerGame" component={SinglePlayerGame} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={navigatorOptions}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SinglePlayerGame"
+          component={SinglePlayerGame}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={Settings}
+          options={{ headerBackTitle: "Home" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,3 @@
 export { default as Home } from './home/home'
 export { default as SinglePlayerGame } from './singlePlayerGame/singlePlayerGame'
+export { default as Settings } from './settings/settings'
